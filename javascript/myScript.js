f1561d545e5949cb0aa3f62d9c3392b65036d5ac
Kamv'elihle Dyantyi <ST10434588@rcconnect.edu.za>
2024-08-28 17:13:12 +0200

    "Updated CSS styles, modified HTML structure, removed files, and refactored JavaScript code."

--- javascript/myScript.js
+++ javascript/myScript.js
@@ -1,5 +1,6 @@
-const rhinoIcon = document.querySelector(".Rhino");
-const navBar = document.querySelector(".nav-bar");
+const hamburgerIcon = document.querySelector(".hamburger-icon");
+const navBar = document.querySelector("nav");
 
-
-  
+hamburgerIcon.addEventListener("click", function() {
+    navBar.classList.toggle("active"); // Toggle the active class on the nav element
+});
